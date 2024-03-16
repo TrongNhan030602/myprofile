c6de457686dfb96d534a409b1e9c3afe26adea86
TrongNhan030602 <ntnhan030602@gmail.com>
2024-03-16 13:15:41 +0700

    Create profile

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.slider-container').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        pauseOnFocus: false
+    });
+});
